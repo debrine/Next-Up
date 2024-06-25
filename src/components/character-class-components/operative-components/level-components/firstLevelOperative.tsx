d7c9b035f081306904a585f1eecc183a0ea2b16c
Col-Ian <100207902+Col-Ian@users.noreply.github.com>
2024-06-25 12:54:02 -0230

    Adding Class features for Operative for character creation.

--- src/components/character-class-components/operative-components/level-components/firstLevelOperative.tsx
+++ src/components/character-class-components/operative-components/level-components/firstLevelOperative.tsx
@@ -1,8 +1,16 @@
+import { useLocalStorage } from "../../../../data/useLocalStorage"
+import styles from './OperativeLevelComponents.module.css'
 
 
 function firstLevelOperative() {
+
+  // Store options saved in local storage temporarily.
+  const [, setOperativeTemp] = useLocalStorage('OperativeTemp')
   return (
-    <div>firstLevelOperative</div>
+    <div className={styles.firstParentDiv}>
+      <h3>Specialization</h3>
+      <p>Your specialization represents your primary area of expertise. Pick one specialisation upon taking your 1st level of the operative class. Once made, this choice cannot be changed. Your specialization grants you the Skill Focus feat (see page 161 of the Core Rulebook) in your specialization's associated skills, and you gain a free skill rank in each of those skills at each operative level (this does not allow you to exceed the maximum number of skill ranks in a single skill).</p>
+    </div>
   )
 }
 
--- src/data/class-information/operative/abilities/operativeAbilityList.ts
+++ src/data/class-information/operative/abilities/operativeAbilityList.ts
@@ -3,16 +3,17 @@
 let operativeAbilityList: {[key:string]:{
     abilityName:string[],
     abilityDescription: string[],
-    hasOptions: boolean,
-    options:string[]
+    actionType: string[],
+    usesResolve: number[]
 }} = {
     '1':{
-        abilityName:['OPERATIVE\'S EDGE (EX)', 'SPECIALIZAION'],
+        abilityName:['OPERATIVE\'S EDGE (EX)', 'SPECIALIZAION', 'TRICK ATTACK [EX]'],
         abilityDescription: [
             'Your diverse training as an operative grants you a +1 insight bonus to initiative checks and to skill checks. This bonus increases by 1 at 3rd level and every 4 levels thereafter.',
-            'Your specialization represents your primary area of expertise. Pick one specialisation upon taking your 1st level of the operative class. Once made, this choice cannot be changed. Descriptions of the specialiations appear on page 94 of the Core Rulebook. Your specialization grants you the Skill Focus feat (see page 161 of the Core Rulebook) in your specialization\'s associated skills, and you gain a free skill rank in each of those skills at each operative level (this does not allow you to exceed the maximum number of skill ranks in a single skill.'
+            'Your specialization represents your primary area of expertise. Pick one specialisation upon taking your 1st level of the operative class. Once made, this choice cannot be changed. Descriptions of the specialiations appear on page 94 of the Core Rulebook. Your specialization grants you the Skill Focus feat (see page 161 of the Core Rulebook) in your specialization\'s associated skills, and you gain a free skill rank in each of those skills at each operative level (this does not allow you to exceed the maximum number of skill ranks in a single skill).',
+            'You can trick or startle a foe and then attack when she drops her guard. As a full action, you can move up to your speed. Whether or not you moved, you can then make an attack with a melee weapon with the operative special property or with any small arm. Just before making your attack, attempt a Bluff, Intimidate, or Stealth check (or a check associated with your specialization; see page 94 of the Core Rulebook) with a DC equal to 20 + your target\'s CR. If you succeed at the check, you deal 1d4 additional damage and the target is flat-footed against your attack. This damage increases to 1d8 at 3rd level, to 3d8 at 5th level, and by an additional 1d8 every 2 levels thereafter. You can\'t use this ability with a weapon that has the unwieldy special property or that requires a full action to make a single attack.'
         ],
-        hasOptions: true,
-        options:['']
+        actionType:['None', 'None', 'Full Action'],
+        usesResolve:[0,0,0]
     }
 }
