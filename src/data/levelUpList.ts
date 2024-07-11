66355c3af61a35d2e7f11a3912838a8cc2f60a6e
Col-Ian <100207902+Col-Ian@users.noreply.github.com>
2024-07-11 13:51:41 -0230

    Adjustments for GitHub Pages.

--- src/data/levelUpList.ts
+++ src/data/levelUpList.ts
@@ -1,6 +1,6 @@
 import FirstLevelOperative from "../components/character-class-components/operative-components/level-components/FirstLevelOperative.tsx";
-
-
+// Deployment failing:
+// Error: src/data/levelUpList.ts(1,33): error TS2307: Cannot find module '../components/character-class-components/operative-components/level-components/FirstLevelOperative.tsx' or its corresponding type declarations.
 
 export const levelUpList: {[key:string]: {[key:string]: {componentForClass: (key:string)=>JSX.Element}}} = {
     '1':{
