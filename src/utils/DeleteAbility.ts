388308aa5e27694f415f25b34f558f98b54468db
Col-Ian <100207902+Col-Ian@users.noreply.github.com>
2024-07-15 14:23:20 -0230

    Added a function to delete abilities.

---
+++ src/utils/DeleteAbility.ts
@@ -0,0 +1,12 @@
+import { getValue } from "./getValue";
+import { setValue } from "./setValue";
+
+export function DeleteAbility(keyID: string, ability: AbilityListTypes) {
+  const newArray: AbilityListTypes[] = getValue(`Abilities${keyID}`);
+
+  const filteredArray = newArray.filter((filteredAbility: AbilityListTypes) => {
+    filteredAbility.abilityName != ability.abilityName;
+  });
+
+  setValue(`Abilities${keyID}`, [...filteredArray]);
+}
