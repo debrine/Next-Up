fd40e48e525c94fe559e2424f973d62b8835983b
Col-Ian <100207902+Col-Ian@users.noreply.github.com>
2024-07-15 10:27:02 -0230

    Changed name of file

--- src/data/levelUpList.ts
+++ src/data/levelUpList.ts
@@ -1,11 +1,15 @@
-import FirstLevelOperative from '../components/character-class-components/operative-components/level-components/FirstLevelOperative.tsx'
+import FirstLevelOperative from "../components/character-class-components/operative-components/level-components/FirstLevelOperative.tsx";
 // Deployment failing:
 // Error: src/data/levelUpList.ts(1,33): error TS2307: Cannot find module '../components/character-class-components/operative-components/level-components/FirstLevelOperative.tsx' or its corresponding type declarations.
 
-export const levelUpList: {[key:string]: {[key:string]: {componentForClass: (key:string)=>JSX.Element}}} = {
-    '1':{
-        'Operative':{
-            componentForClass: FirstLevelOperative
-        }
-    }
-}
+export const levelUpList: {
+  [key: string]: {
+    [key: string]: { componentForClass: (key: string) => JSX.Element };
+  };
+} = {
+  "1": {
+    Operative: {
+      componentForClass: FirstLevelOperative,
+    },
+  },
+};
