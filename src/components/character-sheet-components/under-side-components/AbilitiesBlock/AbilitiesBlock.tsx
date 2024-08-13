fcdd911eddac40779e0050e165b93b9df6ce0d88
Dylan B <dylanbrine@gmail.com>
2024-08-13 19:27:55 -0300

    working

--- src/components/character-sheet-components/under-side-components/AbilitiesBlock/AbilitiesBlock.tsx
+++ src/components/character-sheet-components/under-side-components/AbilitiesBlock/AbilitiesBlock.tsx
@@ -1,102 +1,111 @@
 import SheetLabel from '../../labels/SheetLabel';
 import styles from './AbilitiesBlock.module.css';
 import AddButtonLabel from '../../../character-creation-components/AddButtonLabel/AddButtonLabel';
-import { FieldValues, useFormContext } from 'react-hook-form';
+import { FieldValues, useForm, useFormContext } from 'react-hook-form';
 import { useEffect } from 'react';
 import { useAbilities } from '../../../../hooks/useAbilities';
 
 // Dylan: Works with no infinite render, but not updating dynamically.
 
 type FormValues = FieldValues & {
-	name: AbilityListTypes[];
+  name: AbilityListTypes[];
 };
 
 function AbilitiesBlock() {
-	const { abilitiesArray } = useAbilities();
+  const {
+    abilitiesArray,
+    updateAbilityArray,
+    handleAddAbility,
+    handleDeleteAbility,
+    currentCharacterID,
+  } = useAbilities();
 
-	const { handleAddAbility, handleDeleteAbility } = useAbilities();
+  const { register, watch, reset } = useForm();
+  useEffect(() => {
+    console.log('abilities array in useEffect', abilitiesArray);
+    reset(abilitiesArray);
+  }, [currentCharacterID]);
 
-	const { register } = useFormContext<FormValues>();
+  useEffect(() => {
+    const subscription = watch((data) => {
+      console.log('form update triggered', data);
+      updateAbilityArray(data.abilities);
+    });
+    return () => subscription.unsubscribe();
+  }, [watch, currentCharacterID]);
 
-	useEffect(() => {
-		console.log(abilitiesArray);
-	}, []);
-
-	return (
-		<div className={styles.parentDiv}>
-			<div className={styles.labelDiv}>
-				<SheetLabel sheetLabelText='ABILITIES' />
-				<div
-					className={styles.addAbilityButton}
-					onClick={() => {
-						handleAddAbility();
-					}}
-				>
-					<AddButtonLabel itemToAdd='ABILITY' />
-				</div>
-			</div>
-			<div className={styles.abilitiesBlockContent}>
-				{abilitiesArray.map((ability, index) => {
-					return (
-						<div
-							className={styles.individualAbility}
-							key={`${ability.abilityName}${index}`}
-						>
-							<div
-								className={styles.delete}
-								onClick={() => {
-									handleDeleteAbility(index);
-								}}
-							>
-								&#128465;
-							</div>
-							<div className={styles.topRow}>
-								<div className={styles.inputDiv}>
-									<input
-										type='text'
-										{...register(`abilities.${index}.abilityName`)}
-										defaultValue={abilitiesArray[index].abilityName}
-										spellCheck={false}
-										className={styles.textInput}
-									/>
-								</div>
-								<div className={styles.verticalBar} />
-								<div className={styles.inputDiv}>
-									<input
-										type='text'
-										{...register(`abilities.${index}.abilitySource`)}
-										defaultValue={abilitiesArray[index].abilitySource}
-										spellCheck={false}
-										className={styles.textInput}
-									/>
-								</div>
-								<div className={styles.verticalBar} />
-								<div>TYPE</div>
-								<div className={styles.verticalBar} />
-								<div className={styles.inputDiv}>
-									<div className={styles.inputLabel}>RP</div>
-									<input
-										type='number'
-										{...register(`abilities.${index}.usesResolve`)}
-										defaultValue={abilitiesArray[index].usesResolve}
-										className={styles.numberInput}
-									/>
-								</div>
-							</div>
-							<div className={styles.abilityDescription}>
-								<textarea
-									{...register(`abilities.${index}.abilityDescription`)}
-									defaultValue={abilitiesArray[index].abilityDescription}
-									className={styles.abilityTextarea}
-									spellCheck={false}
-								/>
-							</div>
-						</div>
-					);
-				})}
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.parentDiv}>
+      <div className={styles.labelDiv}>
+        <SheetLabel sheetLabelText='ABILITIES' />
+        <div
+          className={styles.addAbilityButton}
+          onClick={() => {
+            handleAddAbility();
+          }}>
+          <AddButtonLabel itemToAdd='ABILITY' />
+        </div>
+      </div>
+      <div className={styles.abilitiesBlockContent}>
+        {abilitiesArray.map((ability, index) => {
+          return (
+            <div
+              className={styles.individualAbility}
+              key={`${ability.abilityName}${index}`}>
+              <div
+                className={styles.delete}
+                onClick={() => {
+                  handleDeleteAbility(index);
+                }}>
+                &#128465;
+              </div>
+              <div className={styles.topRow}>
+                <div className={styles.inputDiv}>
+                  <input
+                    type='text'
+                    {...register(`abilities.${index}.abilityName`)}
+                    defaultValue={abilitiesArray[index].abilityName}
+                    spellCheck={false}
+                    className={styles.textInput}
+                  />
+                </div>
+                <div className={styles.verticalBar} />
+                <div className={styles.inputDiv}>
+                  <input
+                    type='text'
+                    {...register(`abilities.${index}.abilitySource`)}
+                    defaultValue={abilitiesArray[index].abilitySource}
+                    spellCheck={false}
+                    className={styles.textInput}
+                  />
+                </div>
+                <div className={styles.verticalBar} />
+                <div>TYPE</div>
+                <div className={styles.verticalBar} />
+                <div className={styles.inputDiv}>
+                  <div className={styles.inputLabel}>RP</div>
+                  <input
+                    type='number'
+                    {...register(`abilities.${index}.usesResolve`)}
+                    defaultValue={abilitiesArray[index].usesResolve}
+                    className={styles.numberInput}
+                  />
+                </div>
+              </div>
+              <div className={styles.abilityDescription}>
+                <textarea
+                  {...register(`abilities.${index}.abilityDescription`)}
+                  defaultValue={abilitiesArray[index].abilityDescription}
+                  className={styles.abilityTextarea}
+                  spellCheck={false}
+                />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default AbilitiesBlock;
--- src/hooks/useAbilities.ts
+++ src/hooks/useAbilities.ts
@@ -4,48 +4,51 @@ import { useParams } from 'react-router-dom';
 import { setValue } from '../utils/setValue';
 
 export function useAbilities() {
-	const { characterID } = useParams();
+  const { characterID } = useParams();
 
-	const [abilitiesArray, setAbilitiesArray] = useState<AbilityListTypes[]>(
-		getValue(`Abilities${characterID}`)
-	);
+  const [abilitiesArray, setAbilitiesArray] = useState<AbilityListTypes[]>(
+    getValue(`Abilities${characterID}`)
+  );
+  const [currentCharacterID, setCurrentCharacterID] = useState(characterID);
 
-	useEffect(() => {
-		setAbilitiesArray(getValue(`Abilities${characterID}`));
-	}, [characterID]);
+  useEffect(() => {
+    setAbilitiesArray(getValue(`Abilities${characterID}`));
+    setCurrentCharacterID(characterID);
+  }, [characterID]);
 
-	const updateAbilityArray = useCallback(
-		(newAbilityArray: AbilityListTypes[]) => {
-			setAbilitiesArray(newAbilityArray);
-			setValue(`Abilities${characterID}`, newAbilityArray);
-		},
-		[characterID]
-	);
+  const updateAbilityArray = useCallback(
+    (newAbilityArray: AbilityListTypes[]) => {
+      setAbilitiesArray(newAbilityArray);
+      setValue(`Abilities${characterID}`, newAbilityArray);
+    },
+    [characterID]
+  );
 
-	function handleAddAbility() {
-		updateAbilityArray([
-			...abilitiesArray,
-			{
-				abilityName: '',
-				abilityDescription: '',
-				abilitySource: '',
-				actionType: [''],
-				usesResolve: 0,
-			},
-		]);
-	}
+  function handleAddAbility() {
+    updateAbilityArray([
+      ...abilitiesArray,
+      {
+        abilityName: '',
+        abilityDescription: '',
+        abilitySource: '',
+        actionType: [''],
+        usesResolve: 0,
+      },
+    ]);
+  }
 
-	function handleDeleteAbility(index: number) {
-		const filteredArray = abilitiesArray.filter(
-			(ability) => abilitiesArray.indexOf(ability) != index
-		);
-		updateAbilityArray(filteredArray);
-	}
+  function handleDeleteAbility(index: number) {
+    const filteredArray = abilitiesArray.filter(
+      (ability) => abilitiesArray.indexOf(ability) != index
+    );
+    updateAbilityArray(filteredArray);
+  }
 
-	return {
-		abilitiesArray,
-		updateAbilityArray,
-		handleAddAbility,
-		handleDeleteAbility,
-	};
+  return {
+    abilitiesArray,
+    updateAbilityArray,
+    handleAddAbility,
+    handleDeleteAbility,
+    currentCharacterID,
+  };
 }
